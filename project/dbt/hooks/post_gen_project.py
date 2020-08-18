dd387d6561dbda8d3e317639f400d9a7e1a6bf41
Pascal Knapen <pascal.knapen@dataminded.be>
2020-08-18 15:44:52 +0200

    adding the dbt project

---
+++ project/dbt/hooks/post_gen_project.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import os
+import dbt.task.init as init_task
+from dataclasses import dataclass
+
+project_name = "{{ cookiecutter.project_name }}"
+
+
+@dataclass
+class InitArguments:
+    project_name: str
+
+
+def fix_dbt_project():
+    data = None
+    with open(f'./{project_name}/dbt_project.yml', "rt") as f:
+        data = f.read()
+        data = data.replace('my_new_project', project_name)
+    with open(f'./{project_name}/dbt_project.yml', "wt") as f:
+        f.write(data)
+
+
+def initialize_dbt():
+    os.mkdir('dbt')
+    os.chdir('dbt')
+    os.environ["DBT_PROFILES_DIR"] = os.getcwd()
+    task = init_task.InitTask(args=InitArguments(project_name), config=None)
+    task.run()
+    fix_dbt_project()
+
+
+if __name__ == "__main__":
+    initialize_dbt()
