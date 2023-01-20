5bf6faba907801e76d95d7b8e28e3da2a1b8fe9a
Stijn De Haes <stijndehaes@gmail.com>
2023-01-20 09:25:37 +0100

    Upgrade spark template (#83)

--- project/spark/hooks/pre_gen_project.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-spark_version = "{{ cookiecutter.spark_version }}"
-project_type = "{{ cookiecutter.project_type }}"
-
-
-if __name__ == "__main__":
-    if "streaming" in project_type and spark_version != "3.0":
-        raise Exception("Streaming is only supported when using spark 3")
