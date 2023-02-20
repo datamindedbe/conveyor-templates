c191ab7eb5f27fef493a02175c4ae320204c1522
nclaeys <niels.claeys@gmail.com>
2023-02-20 15:21:27 +0100

    Improve template

--- project/dbt/{{ cookiecutter.project_name }}/dags/{{ cookiecutter.project_name }}.py
+++ project/dbt/{{ cookiecutter.project_name }}/dags/{{ cookiecutter.project_name }}.py
@@ -21,17 +21,20 @@ dag = DAG(
     "{{ cookiecutter.project_name }}", default_args=default_args, schedule_interval="{{ cookiecutter.workflow_schedule }}", max_active_runs=1
 )
 
-#factory = ConveyorDbtTaskFactory(
-#{%- if cookiecutter.conveyor_managed_role %}
-#    task_aws_role="{{ cookiecutter.project_name }}-{% raw %}{{ macros.conveyor.env() }}{% endraw %}",
-#{%- endif %}
-#)
-#factory.add_tasks_to_dag(dag=dag)
+{%- if cookiecutter.database_type == "duckdb" %}
 ConveyorContainerOperatorV2(
     dag=dag,
-    task_id="dbt-duckdb-tutorial",
-    arguments=["build", "--profiles-dir", "/app/dbt", "--project-dir", "/app/dbt/{{ cookiecutter.project_name }}"],
-    {%- if cookiecutter.conveyor_managed_role %}
-       task_aws_role="{{ cookiecutter.project_name }}-{% raw %}{{ macros.conveyor.env() }}{% endraw %}",
-    {%- endif %}
+    task_id="task1",
+    arguments=["build", "--target", "dev"],
+{%- if cookiecutter.conveyor_managed_role == "Yes" %}
+    aws_role="{{ cookiecutter.project_name }}-{% raw %}{{ macros.conveyor.env() }}{% endraw %}",
+{%- endif %}
 )
+{%- else %}
+factory = ConveyorDbtTaskFactory(
+{%- if cookiecutter.conveyor_managed_role == "Yes" %}
+    task_aws_role="{{ cookiecutter.project_name }}-{% raw %}{{ macros.conveyor.env() }}{% endraw %}",
+{%- endif %}
+)
+factory.add_tasks_to_dag(dag=dag)
+{% endif -%}
--- tests/projects/test_dbt.py
+++ tests/projects/test_dbt.py
@@ -4,6 +4,11 @@ import os
 from project.dbt.hooks.post_gen_project import initialize_dbt_in_dir
 
 
+def assertFileContainsContent(file, content):
+    fileContent = open(file).read()
+    assert fileContent.__contains__(content)
+
+
 def test_dbt_template(cookies):
     result = cookies.bake(
         template=f"{os.path.dirname(os.path.abspath(__file__))}/../../project/dbt",
@@ -12,6 +17,62 @@ def test_dbt_template(cookies):
     assert 0 == result.exit_code
     assert result.exception is None
     assert result.project_path.is_dir()
+
+
+def test_dbt_duckdb_template(cookies):
+    result = cookies.bake(
+        template=f"{os.path.dirname(os.path.abspath(__file__))}/../../project/dbt",
+        extra_context={"database_type": "duckdb", "project_name": "duckdb_test"},
+    )
+    assert 0 == result.exit_code
+    assert result.exception is None
+    assertFileContainsContent(
+        result.project + "/dags/duckdb_test.py", "ConveyorContainerOperatorV2("
+    )
+
+
+def test_dbt_duckdb_not_conveyor_managed_template(cookies):
+    result = cookies.bake(
+        template=f"{os.path.dirname(os.path.abspath(__file__))}/../../project/dbt",
+        extra_context={
+            "database_type": "duckdb",
+            "project_name": "duckdb_test",
+            "conveyor_managed_role": "No",
+        },
+    )
+    assert 0 == result.exit_code
+    assert result.exception is None
+    file = result.project + "/dags/duckdb_test.py"
+    file_content = file.read()
+    assert not file_content.__contains__("aws_role")
+
+
+def test_dbt_duckdb_conveyor_managed_template(cookies):
+    result = cookies.bake(
+        template=f"{os.path.dirname(os.path.abspath(__file__))}/../../project/dbt",
+        extra_context={
+            "database_type": "duckdb",
+            "project_name": "duckdb_test",
+            "conveyor_managed_role": "Yes",
+        },
+    )
+    assert 0 == result.exit_code
+    assert result.exception is None
+    file = result.project + "/dags/duckdb_test.py"
+    file_content = file.read()
+    assert file_content.__contains__("aws_role")
+
+
+def test_dbt_postgres_template(cookies):
+    result = cookies.bake(
+        template=f"{os.path.dirname(os.path.abspath(__file__))}/../../project/dbt",
+        extra_context={"database_type": "postgres", "project_name": "postgres_test"},
+    )
+    assert 0 == result.exit_code
+    assert result.exception is None
+    assertFileContainsContent(
+        result.project + "/dags/postgres_test.py", "factory.add_tasks_to_dag("
+    )
 
 
 def test_dbt_template_azure(cookies):
